798816f89ecc350b65ee844fa39c3b8352e08b2f
Sveta-Pneva <101118701+Sveta-Pneva@users.noreply.github.com>
2022-03-25 17:01:49 +0300

    Add files via upload
    Домашняя работа 1, упражнение 2, пункт b. Пнева Света

---
+++ 1_урок_дз_2_пункт_b.py
@@ -0,0 +1,12 @@
+answer = 0
+num_cube = [i**3 + 17 for i in range(1, 1001, 2)]
+for i in range(500):
+    num = num_cube[i]
+    num_copy = num
+    sum_num = 0
+    while num != 0:
+        sum_num += num % 10
+        num //= 10
+    if sum_num % 7 == 0:
+        answer += num_copy
+print(answer)
