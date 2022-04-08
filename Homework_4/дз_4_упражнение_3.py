2085a819abfb9d7b3334e44e8f5c0efe09ac5fdc
Sveta_Pneva <pnevasvetlana3@gmail.com>
2022-04-08 18:36:58 +0300

    Homework 4

---
+++ Homework_4/дз_4_упражнение_3.py
@@ -0,0 +1,33 @@
+import requests
+import xml.dom.minidom as xml
+from datetime import datetime
+
+def currency_rates(n):
+    n = n.upper()
+    url = 'http://www.cbr.ru/scripts/XML_daily.asp' #09.04.2022
+    res = requests.get(url)
+    res = res.text
+    dom = xml.parseString(res)
+    date = res[60:70]
+    date = datetime.strptime(date, "%d.%m.%Y")
+    elements = dom.getElementsByTagName("Valute")
+    result = {}
+    for i in elements:
+        for j in i.childNodes:
+            if j.tagName == 'Value':
+                if j.firstChild.nodeType == 3:
+                    value = float(j.firstChild.data.replace(',', '.'))
+            elif j.tagName == 'CharCode':
+                if j.firstChild.nodeType == 3:
+                    char_code = j.firstChild.data
+        if char_code == n:
+            result[char_code] = value
+            for key in result.keys():
+                result = key + " " + str(result[key]) + ", " + str(date)
+            return result
+            break
+    return None
+
+if __name__ == '__main__':
+    cur_code = input()
+    print(currency_rates(cur_code))
